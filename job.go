44afcd2a44c64ef253edd93b5438ff5b7be1d793
Sayan <shayan1337@gmail.com>
2019-07-22 20:57:38 +0530

    Create a job type that consists of a requestId and request URL

--- job.go
+++ job.go
@@ -1,1 +1,16 @@
 package worker_pool
+
+type Job struct {
+	requestId int
+	url string
+}
+
+func (this Job) Url() string {
+	return this.url
+}
+
+func (this Job) RequestId() int {
+	return this.requestId
+}
+
+
