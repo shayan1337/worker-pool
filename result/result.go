5a0884d2e55ec59c54430b47a6bc637f5ce27e97
Sayan <shayan1337@gmail.com>
2019-07-23 11:22:55 +0530

    Modify result type to encapsulate error as well

--- result/result.go
+++ result/result.go
@@ -2,18 +2,31 @@ package result
 
 import (
 	"net/http"
-	"worker-pool/job"
+	. "worker-pool/job"
 )
 
 type Result struct {
-	job job.Job
-	response http.Response
+	job Job
+	response *http.Response
+	err error
+}
+
+func NewResult(job Job, response *http.Response, err error) Result {
+	return Result{
+		job: job,
+		response: response,
+		err: err,
+	}
+}
+
+func (this Result) Err() error {
+	return this.err
 }
 
 func (this Result) Response() http.Response {
 	return this.response
 }
 
-func (this Result) Jon() job.Job {
+func (this Result) Jon() Job {
 	return this.job
 }
