8b57eda5e0c901779232b6cb078c8c2e99d8f4ef
Sayan <shayan1337@gmail.com>
2019-07-24 21:18:44 +0530

    Use a channel instead of a waitgroup to block the main routine until the others return

--- main.go
+++ main.go
@@ -15,17 +15,16 @@ var results = make(chan Result, 5)
 var urls = []string{"http://facebook.com", "http://instagram.com", "http://linkedin.com", "http://stackoverflow.com", "http://golang.org", "http://helloworld.com", "http://somewebsite.com", "http://ihaverunoutofwebsites.com"}
 
 func main() {
-	var wg sync.WaitGroup
+	done := make(chan bool)
 	go createWorkers(3)
-	wg.Add(1)
-	go display(&wg)
+	go display(done)
 	requestId := 1
 	for _, url := range urls {
 		jobs <- NewJob(requestId, url)
 		requestId = requestId + 1
 	}
 	close(jobs)
-	wg.Wait()
+	<-done
 }
 
 func createWorkers(numberOfWorkers int) {
@@ -46,7 +45,7 @@ func work(wg *sync.WaitGroup) {
 	wg.Done()
 }
 
-func display(wg *sync.WaitGroup) {
+func display(done chan bool) {
 	for result := range results {fmt.Println("----------------------------")
 		fmt.Println(fmt.Sprintf("Response from %s is :::::", result.Job().Url()))
 		if result.Err() != nil {
@@ -55,5 +54,5 @@ func display(wg *sync.WaitGroup) {
 		}
 		log.Println(result.Response())
 	}
-	wg.Done()
+	done <- true
 }
